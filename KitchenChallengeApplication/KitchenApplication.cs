47572c6a2af7dfef8e39a880573569eff77714e6
Ramon Felipe Alves de Arruda Silva <ramon.silva@euemdia.com.br>
2020-12-18 19:36:07 -0300

    refactor(kitchenApplication): preparing order
    -creation of preparingOrderItem method
    -adding total preparation time per order

--- KitchenChallengeApplication/KitchenApplication.cs
+++ KitchenChallengeApplication/KitchenApplication.cs
@@ -1,10 +1,10 @@
 ﻿using KitchenChallenge.Application.Factory;
-using KitchenChallenge.Domain.Areas;
 using KitchenChallenge.Domain.Order;
 using KitchenChallengeApplication.Interfaces;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,39 +22,55 @@ namespace KitchenChallengeApplication
 
         public async Task PrepareOrdersAsync(Queue<Order> ordersQueue)
         {
+
             _logger.LogInformation("Preparing order");
             var exceptions = new List<Exception>();
-            List<Task> orderItemsPreparation = new List<Task>();
+            Stopwatch regularSW = new Stopwatch();
+            regularSW.Start();
 
             while (ordersQueue.Count > 0)
             {
-                var order = ordersQueue.Peek();
-                order.Number = OrderNumber++;
+                var order = ordersQueue.Dequeue();
                 
                 try
                 {
-                    foreach (var item in order.Items)
-                    {
-                        var kitchenArea = KitchenFactory.GetKitchenArea(item.Type);
-
-                        orderItemsPreparation.Add(kitchenArea.PrepareItemAsync(item));
-                    }
-
-                    await Task.WhenAll(orderItemsPreparation);
-
-                    _logger.LogInformation($"Order ({order.Number}) done!.\n{order}");
+                    await PrepareOrderItemsAsync(order);
                 }
                 catch (Exception e)
                 {
-                    _logger.LogInformation($"An error occurred with order {order.Number}.\n{order}.\n({e.Message})");
+                    _logger.LogError($"An error occurred with order {order.Number}.\n{order}.\n({e.Message})");
                     exceptions.Add(e);
                 }
-
-                ordersQueue.Dequeue();
             }
+
+            regularSW.Stop();
+            _logger.LogInformation($"Total time spent: {regularSW.ElapsedMilliseconds} (ms)");
 
             if (exceptions.Any())
                 throw new AggregateException(exceptions);
+        }
+
+        private async Task<Order> PrepareOrderItemsAsync(Order order)
+        {
+            Stopwatch regularSW = new Stopwatch();
+            regularSW.Start();
+
+            order.SetOrderNumber(OrderNumber++);
+            List<Task> orderItemsPreparation = new List<Task>();
+            
+            foreach (var item in order.Items)
+            {
+                var kitchenArea = KitchenFactory.GetKitchenArea(item.Type);
+                orderItemsPreparation.Add(kitchenArea.PrepareItemAsync(item));
+            }
+
+            await Task.WhenAll(orderItemsPreparation);
+            _logger.LogInformation($"Order ({order.Number}) done!.\n{order}");
+            
+            regularSW.Stop();
+            _logger.LogInformation($"Total time spent: {regularSW.ElapsedMilliseconds} (ms)");
+
+            return order;
         }
     }
 }
